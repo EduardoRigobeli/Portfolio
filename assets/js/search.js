3acd4f7e6a608cb3352ad7d12f26a8c2c7630e5a
Eduardo Rigobeli <eduardo.souza@grupointegrado.br>
2025-01-17 22:05:30 -0300

    Formulario de Pesquisa pronto

---
+++ assets/js/search.js
@@ -0,0 +1,8 @@
+const btnEnviar = document.getElementById('enviar');
+
+btnEnviar.addEventListener('click', () => {
+    const name = document.getElementById('input-name');
+    let nome = name.value;
+
+    alert("O formulário não ira enviar nada. Mas obrigado por testar/tentar o envio das informações.")
+});
