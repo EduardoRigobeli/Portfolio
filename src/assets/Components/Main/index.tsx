701ac17a31db2fe56f3f8527675c484f26ef3ccb
Eduardo Rigobeli <eduardo.souza@grupointegrado.br>
2025-04-07 22:28:38 -0300

    Início de Main

---
+++ src/assets/Components/Main/index.tsx
@@ -0,0 +1,15 @@
+import styles from './main.module.css'
+
+function Main() {
+    return (
+        <div className={styles.container}>
+            <div className="content">
+                <span className="blur"></span>
+                <span className="blur"></span>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Main
