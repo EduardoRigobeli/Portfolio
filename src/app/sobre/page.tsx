41e567c9c207529de0acaae6cb050cea679eb4da
Eduardo Rigobeli <eduardo.souza@grupointegrado.br>
2025-07-15 12:33:09 -0300

    Inicio de SOBRE

--- src/app/sobre/page.tsx
+++ src/app/sobre/page.tsx
@@ -1,7 +1,14 @@
 export default function Sobre() {
     return (
-        <>
-            SOBRE
-        </>
+        <div className="flex items-center justify-center h-[300px]">
+            <section className="text-white flex items-center gap-5">
+                <p>TEXTO</p>
+                <img src="/Perfil.jpg" 
+                     alt="Eduardo Rigobeli"
+                     title="Eduardo Rigobeli"
+                     className="w-[200px] h-[40px] object-cover"
+                     />
+            </section>
+        </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,32 @@
 import Link from "next/link";
 
 export default function Header() {
-    return (
-        <header className="flex items-center justify-center h-25">
-            <ul className="flex gap-8 text-white border-2 border-gray-200 p-3.5 rounded-full backdrop-blur-md">
-                <li>
-                    <Link href="/" className="">Início</Link>
-                </li>
-                <li>
-                    <Link href="/sobre" className="">Sobre</Link>
-                </li>
-                <li>
-                    <Link href="/projetos" className="">Projetos</Link>
-                </li>
-                <li>
-                    <Link href="/contato" className="">Contato</Link>
-                </li>
-            </ul>
-        </header>
-    )
+  return (
+    <header className="flex items-center justify-center h-25">
+      <nav className="text-white">
+        <ul className="flex gap-8 border-2 border-gray-200 p-3.5 rounded-full backdrop-blur-md">
+          <li>
+            <Link href="/" className="">
+              Início
+            </Link>
+          </li>
+          <li>
+            <Link href="/sobre" className="">
+              Sobre
+            </Link>
+          </li>
+          <li>
+            <Link href="/projetos" className="">
+              Projetos
+            </Link>
+          </li>
+          <li>
+            <Link href="/contato" className="">
+              Contato
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
