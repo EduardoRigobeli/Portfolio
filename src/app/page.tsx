a8adb95c31cf9ad894308d933b106c5f6d52a59f
Eduardo Rigobeli <eduardo.souza@grupointegrado.br>
2025-07-15 12:07:52 -0300

    Animação de BEM VINDO pronta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ const firaCode = Fira_Code({
 export default function Home() {
   return (
     <div className="pl-105 pt-20">
-      <h1 className={`${firaCode.className} text-white font-medium text-8xl cursor-default whitespace-nowrap overflow-hidden w-[9.5ch] border-r-[20px]`}>BEM VINDO</h1>
+      <h1 className={`${firaCode.className} text-white font-medium text-8xl cursor-default whitespace-nowrap overflow-hidden w-[9.6ch] border-r-[20px] animate-t-b`}>BEM VINDO</h1>
       <p className="text-white font-light w-[500px] text-[18px]">Conheça mais sobre mim e explore meus projetos pessoais. Vamos criar algo juntos?</p>
     </div>
   );
